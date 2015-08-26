66b0adc3383f36d30ce68694763d18708340586a
sono123 <steveono@gmail.com>
2015-08-26 00:41:52 -0400

    Add decrement functionality after item removal.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	alert('Click "NEW ITEM" to items. The sum feature works too.');
+	// alert('Click "NEW ITEM" to items. The sum feature works too.');
 
 	function sumNumbers() {
 		var totalSum = 0;
@@ -9,6 +9,14 @@ $(document).ready(function(){
 		})
 		return totalSum
 	}
+
+	function update() {
+    	var counter = $(".item_divs").size();
+
+        $(".item_divs span").each(function( index ) {
+        	$(this).text(index + 1);
+        });
+    };
 
     $(document).on('click', 'button', function(){
     	var count = $(".item_divs").size() + 1;
@@ -20,12 +28,19 @@ $(document).ready(function(){
     });
 
     $(document).on('click', 'div.x', function(){
-        $(this).closest(".item_divs").slideUp('normal', function() { $(this).remove() } );
+        $(this).closest(".item_divs").slideUp('normal', function(){ 
+        	$(this).remove();
+        	update();
+        });
     });
+
+
+    // $(document).bind("DOMNodeRemoved", function(){
+    // 	$("#sum_number").text(sumNumbers());
+    // });
 
     $(document).on('click', document, function(){
     	$("#sum_number").text(sumNumbers());
-    	// $("body")[0].click(); not working...
     });
 
 });
